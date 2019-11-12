e235d6357b87e562ea83e51d3558e79b157f555e
Brian Crafton <crafton.b@husky.neu.edu>
2019-11-12 17:03:13 -0500

    gets 85%

--- cifar10_tf_densenet.py
+++ cifar10_tf_densenet.py
@@ -1,9 +1,27 @@
 
+import argparse
 import os
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]='4'
+import sys
 
-####################################
+##############################################
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--epochs', type=int, default=100)
+parser.add_argument('--batch_size', type=int, default=50)
+parser.add_argument('--lr', type=float, default=1e-2)
+parser.add_argument('--gpu', type=int, default=2)
+args = parser.parse_args()
+
+if args.gpu >= 0:
+    os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"]=str(args.gpu)
+
+##############################################
+
+assert((50000 % args.batch_size) == 0)
+assert((10000 % args.batch_size) == 0)
+
+##############################################
 
 import numpy as np
 import tensorflow as tf
@@ -28,18 +46,30 @@ y_test = keras.utils.to_categorical(y_test, 10)
 
 ####################################
 
-epochs = 10
-batch_size = 50
-x = tf.placeholder(tf.float32, [None, 32 , 32 , 3])
+x = tf.placeholder(tf.float32, [None, 32, 32, 3])
 y = tf.placeholder(tf.float32, [None, 10])
 
 ####################################
 
 def batch_norm(x, f, name, vars_dict):
-    gamma = tf.Variable(np.ones(shape=f), dtype=tf.float32)
-    beta = tf.Variable(np.zeros(shape=f), dtype=tf.float32)
-    vars_dict[name + '_gamma'] = gamma
-    vars_dict[name + '_beta'] = beta
+
+    gamma_name = name + '_gamma'
+    beta_name = name + '_beta'
+
+    if gamma_name in vars_dict.keys():
+        gamma = tf.Variable(vars_dict[gamma_name], trainable=False, dtype=tf.float32)
+    else:
+        gamma = tf.Variable(np.ones(shape=f), dtype=tf.float32)
+
+    if beta_name in vars_dict.keys():
+        beta = tf.Variable(vars_dict[beta_name], trainable=False, dtype=tf.float32)
+    else:
+        beta = tf.Variable(np.zeros(shape=f), dtype=tf.float32)
+
+    vars_dict[gamma_name] = gamma
+    vars_dict[beta_name] = beta
+
+    ########################################
 
     mean = tf.reduce_mean(x, axis=[0,1,2])
     _, var = tf.nn.moments(x - mean, axes=[0,1,2])
@@ -47,8 +77,15 @@ def batch_norm(x, f, name, vars_dict):
     return bn
 
 def conv_block(x, f, name, vars_dict):
-    filters = tf.Variable(init_filters(size=f, init='alexnet'), dtype=tf.float32)
+
+    if name in vars_dict.keys():
+        filters = tf.Variable(vars_dict[name], trainable=False, dtype=tf.float32)
+    else:
+        filters = tf.Variable(init_filters(size=f, init='alexnet'), dtype=tf.float32)
+
     vars_dict[name] = filters
+
+    ##################################
 
     fh, fw, fc, fo = f
 
@@ -75,10 +112,8 @@ def dense_block(x, nfmap, k, name, vars_dict):
     conv2 = conv_block(conv1, [3,3,4*k,k], name + '_conv3x3', vars_dict)
     return conv2
 
-# checkout ssdfa/lib/DenseBlock for this one.
-def dense_model(x, xshape, k, block_sizes, name):
-    vars_dict = {}
-    _,_,_,c = xshape
+def dense_model(x, xshape, k, block_sizes, name, vars_dict):
+    _, _, _, c = xshape
 
     blocks = [x]
     for ii in range(len(block_sizes)):
@@ -91,17 +126,22 @@ def dense_model(x, xshape, k, block_sizes, name):
         pool = tf.nn.avg_pool(concat(blocks), ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
         blocks = [pool]
 
-    return blocks[-1], vars_dict
+    return blocks[-1]
 
 ####################################
 
-block_sizes = [4, 4, 4]
+block_sizes = [4, 4, 4, 4]
 k = 32
 nhidden = 3 + k * sum(block_sizes)
 
-dense, vars_dict = dense_model(x, [batch_size, 32, 32, 3], k, block_sizes, 'dense')
-pool  = tf.nn.avg_pool(dense, ksize=[1,4,4,1], strides=[1,4,4,1], padding='SAME')  # 4 -> 1
-flat  = tf.reshape(pool, [batch_size, nhidden])
+try:
+    vars_dict = np.load('cifar10_densenet.npy', allow_pickle=True).item()
+except:
+    vars_dict = {}
+
+dense = dense_model(x, [args.batch_size, 32, 32, 3], k, block_sizes, 'dense', vars_dict)
+pool  = tf.nn.avg_pool(dense, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME') # 4 -> 1
+flat  = tf.reshape(pool, [args.batch_size, nhidden])
 out   = fc_block(flat, [nhidden, 10])
 
 ####################################
@@ -114,7 +154,7 @@ loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=out)
 params = tf.trainable_variables()
 grads = tf.gradients(loss, params)
 grads_and_vars = zip(grads, params)
-train = tf.train.AdamOptimizer(learning_rate=1e-2, epsilon=1.).apply_gradients(grads_and_vars)
+train = tf.train.AdamOptimizer(learning_rate=args.lr, epsilon=1.).apply_gradients(grads_and_vars)
 
 ####################################
 
@@ -123,33 +163,42 @@ config.gpu_options.allow_growth=True
 sess = tf.Session(config=config)
 sess.run(tf.global_variables_initializer())
 
+# need to stuff all this stuff into a function.
+
+# can we call sess.end() or something like that ? 
+# https://github.com/tensorflow/tensorflow/issues/19731
+# https://github.com/tensorflow/tensorflow/issues/17048
+# these threads suggesting we cannot do that.
+
+# really shouldnt have to tho, as long as we can call an assign op or something
+# yeah but then we cant just make something trainable=False
+# wud have to intelligently pick which to train.
+# might be best to just run a new python script each time.
+
 ####################################
 
-for ii in range(epochs):
-    for jj in range(0, 50000, batch_size):
+for ii in range(args.epochs):
+    for jj in range(0, 50000, args.batch_size):
         s = jj
-        e = jj + batch_size
+        e = jj + args.batch_size
         xs = x_train[s:e]
         ys = y_train[s:e]
         sess.run([train], feed_dict={x: xs, y: ys})
         
     total_correct = 0
 
-    for jj in range(0, 10000, batch_size):
+    for jj in range(0, 10000, args.batch_size):
         s = jj
-        e = jj + batch_size
+        e = jj + args.batch_size
         xs = x_test[s:e]
         ys = y_test[s:e]
         _sum_correct = sess.run(sum_correct, feed_dict={x: xs, y: ys})
         total_correct += _sum_correct
 
-    '''
-    param = sess.run(vars_dict, feed_dict={})
-    print (param.keys())
-    '''
-  
     print ("acc: " + str(total_correct * 1.0 / 10000))
         
+weights = sess.run(vars_dict, feed_dict={})
+np.save('cifar10_densenet', weights)
         
 ####################################
 
