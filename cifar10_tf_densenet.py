d82b316fcb759e78da43581bf7bd37a7d1ce9dba
Brian Crafton <crafton.b@husky.neu.edu>
2019-11-12 09:48:30 -0500

    vars in dict

--- cifar10_tf_densenet.py
+++ cifar10_tf_densenet.py
@@ -29,26 +29,31 @@ y_test = keras.utils.to_categorical(y_test, 10)
 ####################################
 
 epochs = 10
-batch_size = 50
+batch_size = 8
 x = tf.placeholder(tf.float32, [None, 32 , 32 , 3])
 y = tf.placeholder(tf.float32, [None, 10])
 
 ####################################
 
-def batch_norm(x, f):
+def batch_norm(x, f, name, vars_dict):
     gamma = tf.Variable(np.ones(shape=f), dtype=tf.float32)
     beta = tf.Variable(np.zeros(shape=f), dtype=tf.float32)
+    vars_dict[name + '_gamma'] = gamma
+    vars_dict[name + '_beta'] = beta
+
     mean = tf.reduce_mean(x, axis=[0,1,2])
     _, var = tf.nn.moments(x - mean, axes=[0,1,2])
     bn = tf.nn.batch_normalization(x=x, mean=mean, variance=var, offset=beta, scale=gamma, variance_epsilon=1e-3)
     return bn
 
-def conv_block(x, f):
-    fh, fw, fc, fo = f
+def conv_block(x, f, name, vars_dict):
     filters = tf.Variable(init_filters(size=f, init='alexnet'), dtype=tf.float32)
+    vars_dict[name] = filters
+
+    fh, fw, fc, fo = f
 
     conv = tf.nn.conv2d(x, filters, [1,1,1,1], 'SAME')
-    bn   = batch_norm(conv, fo)
+    bn   = batch_norm(conv, fo, name, vars_dict)
     relu = tf.nn.relu(bn)
 
     return relu
@@ -65,13 +70,14 @@ def fc_block(x, size):
 def concat(blocks):
     return tf.concat(blocks, axis=3)
 
-def dense_block(x, nfmap, k):
-    conv1 = conv_block(x,     [1,1,nfmap,4*k])
-    conv2 = conv_block(conv1, [3,3,4*k,k])
+def dense_block(x, nfmap, k, name, vars_dict):
+    conv1 = conv_block(x,     [1,1,nfmap,4*k], name + '_conv1x1', vars_dict)
+    conv2 = conv_block(conv1, [3,3,4*k,k], name + '_conv3x3', vars_dict)
     return conv2
 
 # checkout ssdfa/lib/DenseBlock for this one.
-def dense_model(x, xshape, k, block_sizes):
+def dense_model(x, xshape, k, block_sizes, name):
+    vars_dict = {}
     _,_,_,c = xshape
 
     blocks = [x]
@@ -79,19 +85,21 @@ def dense_model(x, xshape, k, block_sizes):
         block_size = block_sizes[ii]
         for jj in range(block_size):
             nfmap = c + k * (sum(block_sizes[0:ii]) + jj)
-            block = dense_block(concat(blocks), nfmap, k)
+            block = dense_block(concat(blocks), nfmap, k, name + '_block%d' % (nfmap), vars_dict) # block_ii_jj -> ii can equal jj
             blocks.append(block)
 
         pool = tf.nn.avg_pool(concat(blocks), ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
         blocks = [pool]
 
-    return blocks[-1]
+    return blocks[-1], vars_dict
 
 ####################################
 
-nhidden = 3 + 32 * 12
+block_sizes = [4, 4, 4]
+k = 32
+nhidden = 3 + k * sum(block_sizes)
 
-dense = dense_model(x, [batch_size, 32, 32, 3], 32, [4, 4, 4])
+dense, vars_dict = dense_model(x, [batch_size, 32, 32, 3], k, block_sizes, 'dense')
 pool  = tf.nn.avg_pool(dense, ksize=[1,4,4,1], strides=[1,4,4,1], padding='SAME')  # 4 -> 1
 flat  = tf.reshape(pool, [batch_size, nhidden])
 out   = fc_block(flat, [nhidden, 10])
@@ -118,7 +126,7 @@ sess.run(tf.global_variables_initializer())
 ####################################
 
 for ii in range(epochs):
-    for jj in range(0, 50000, batch_size):
+    for jj in range(0, 10 * batch_size, batch_size):
         s = jj
         e = jj + batch_size
         xs = x_train[s:e]
@@ -127,13 +135,16 @@ for ii in range(epochs):
         
     total_correct = 0
 
-    for jj in range(0, 10000, batch_size):
+    for jj in range(0, 10 * batch_size, batch_size):
         s = jj
         e = jj + batch_size
         xs = x_test[s:e]
         ys = y_test[s:e]
         _sum_correct = sess.run(sum_correct, feed_dict={x: xs, y: ys})
         total_correct += _sum_correct
+
+    param = sess.run(vars_dict, feed_dict={})
+    print (param.keys())
 
     '''
     param = sess.run(params, feed_dict={})
